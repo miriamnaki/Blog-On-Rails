da6bb8c6aab56d02c142c8bdfa59f902885273ff
MIRIAM NAKI <nakiyingim@gmail.com>
2021-02-04 07:26:18 -0800

    user sign in and sign up added

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :users, only:[:new, :create,:edit, :update]
+  resource :session,only:[:new, :create, :destroy]
+
+
 end
